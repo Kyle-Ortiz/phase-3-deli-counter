94bcdc027f1d56c8c39055cf0c99418f86433e9b
Kyle Ortiz <kyleaortiz@gmail.com>
2021-09-06 13:13:36 -0500

    commit tree e54b19d5c227b1d777f696a63404b7580016383b

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+     if array.length == 0 
+          puts "The line is currently empty."
+     else 
+          name_string = ""
+          array.each.with_index do |name, index|
+               count = index+1
+          name_string = name_string + "#{count}. #{name} "
+          end
+
+          puts "The line is currently: #{name_string[0...-1]}"
+     end
+end
+
+def take_a_number(line,name)
+     line.push(name)
+     puts "Welcome, #{name}. You are number #{line.length} in line."
+end
+
+def now_serving(line)
+     if line.length == 0 
+          puts "There is nobody waiting to be served!"
+     else 
+          puts "Currently serving #{line[0]}."
+          line.shift()
+     end
+end
